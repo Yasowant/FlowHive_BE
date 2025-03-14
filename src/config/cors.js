5d10bb79514d9de9c5ea66f80fd35dc53483821f
Yasowant <yasowant1998@gmail.com>
2025-03-14 08:40:46 +0530

    updated code origin setup

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,19 @@
 const cors = require('cors');
 require('dotenv').config();
 
+const allowedOrigins = [
+  process.env.CLIENT_ORIGIN_LOCAL,
+  process.env.CLIENT_ORIGIN_PRODUCTION,
+];
+
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN_LOCAL, // Allow only local origin
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies and credentials
